fb0485d63e57543f67f852d0fc336b78ad705fb4
Luiz Gustavo <cavalcantelg7@gmail.com>
2026-09-19 13:59:21 -0300

    feat: create test_boto3 file to test the connection with aws3

---
+++ data_to_s3/test_boto3.py
@@ -0,0 +1,15 @@
+import os
+import boto3
+from dotenv import load_dotenv
+
+load_dotenv()
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_REGION")
+)
+
+response = s3.list_objects_v2(Bucket=os.getenv("S3_BUCKET_NAME"))
+print(response)
