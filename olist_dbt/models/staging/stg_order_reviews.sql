a208748b8656b7888d85be50ee40daa489a29f84
Luiz Gustavo <cavalcantelg7@gmail.com>
2026-09-26 14:10:34 -0300

    include the comment_message on staging layer

--- olist_dbt/models/staging/stg_order_reviews.sql
+++ olist_dbt/models/staging/stg_order_reviews.sql
@@ -9,6 +9,7 @@ ajust as (
         order_id,
         review_score,
         review_comment_title as review_comment_title,
+        review_comment_message,
         review_creation_date::timestamp as review_creation_date,
         review_answer_timestamp::timestamp as review_answer_timestamp
     from source
